1c99dfeb822274bf7d38bdd9a1e1cc930191569e
rslabon <rslabon@gmail.com>
2025-06-07 17:35:17 +0200

    day8 - done

---
+++ src/aoc2015/day8.py
@@ -0,0 +1,39 @@
+with open("../../resources/day8.txt", "r") as f:
+    lines = f.read().splitlines()
+
+
+# lines = ['""', '"abc"', '"aaa\\"aaa"', '"\\x27"']
+
+
+def chars(s):
+    return len(eval(s))
+
+
+def codes(s):
+    return len(s)
+
+
+def encode_codes(s):
+    ss = s.replace("\\", "\\\\").replace("\\x", "\\x").replace("\"", "\\\"")
+    # print(s, f"\"{ss}\"")
+    return len(f"\"{ss}\"")
+
+
+def part1():
+    total = 0
+    for line in lines:
+        total += codes(line) - chars(line)
+
+    print(total)
+
+
+def part2():
+    total = 0
+    for line in lines:
+        total += encode_codes(line) - codes(line)
+
+    print(total)
+
+
+part1()
+part2()
